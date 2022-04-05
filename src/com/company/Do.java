ca638fc001afaf94749fb01de33b9a3433b9cb85
Kristan Frost <kristan.frost@gmail.com>
2022-04-05 14:14:32 -0500

    Updated a couple examples

--- src/com/company/Do.java
+++ src/com/company/Do.java
@@ -10,13 +10,6 @@ public class Do {
         // this is a little different
 
         // a do-while loop always executes at least one time
-//        i = 400;
-//        do {
-//            System.out.println(i);
-//            i++;
-//        }
-//        while(i > 500);
-
         // do while loops exist in both JS and Java. Again good to practice in Unit 2, but not nearly as important to understand as while loops, or for loops
     }
 }
