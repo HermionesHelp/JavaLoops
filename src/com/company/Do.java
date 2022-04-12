2b1342c6216a5e0b3f227bd32285a1861c905334
Kristan Frost <kristan.frost@gmail.com>
2022-04-12 12:38:14 -0500

    small changes to the loops

--- src/com/company/Do.java
+++ src/com/company/Do.java
@@ -2,14 +2,13 @@ package com.company;
 
 public class Do {
     public static void main(String[] args) {
+
         int i = 0;
+
         do {
             System.out.println(i);
             i++;
         } while (i < 10);
-        // this is a little different
 
-        // a do-while loop always executes at least one time
-        // do while loops exist in both JS and Java. Again good to practice in Unit 2, but not nearly as important to understand as while loops, or for loops
     }
 }
--- src/com/company/For.java
+++ src/com/company/For.java
@@ -2,10 +2,10 @@ package com.company;
 
 public class For {
     public static void main(String[] args) {
-        // your standard run of the mill for loop:
+
         for(int i = 0; i < 10; i++) {
             System.out.println(i);
         }
-        // that looks familiar
+
     }
 }
